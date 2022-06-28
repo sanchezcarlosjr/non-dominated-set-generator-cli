4720813c5106ea330abb09836834b65a1e0d5579
Carlos Eduardo Sanchez Torres (sanchezcarlosjr) <hello@sanchezcarlosjr.com>
2022-06-28 10:32:41 -0500

    fix: remove directory because it is not flex

--- src/app/file_view.py
+++ src/app/file_view.py
@@ -1,7 +1,6 @@
 class FileView:
     def __init__(self, file, pareto_front):
         self.file = file
-        self.directory = "generated-non-dominated-sets"
         self.pareto_front = [" ".join(row) for row in pareto_front.astype(str)]
         self.rows = pareto_front.shape[0]
         self.columns = pareto_front.shape[1]
@@ -9,7 +8,7 @@ class FileView:
             raise Exception("Your parameters has generated an invalid space. Try to decrease translation.")
 
     def make(self):
-        with open(f"{self.directory}/{self.file}", "w") as file:
+        with open(f"{self.file}", "w") as file:
             file.write(f"# {self.rows} {self.columns} \n")
             for row in self.pareto_front:
                 file.write(f"{row} \n")
